4860baf910d417e999100d61861379bf3be5f60c
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-05-06 16:54:09 +0800

    finished Beauty of struggle blogs

---
+++ app/blog/BeautyofTheStruggle/page.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import React from "react";
+import PageTransition from "@/components/Global/PageTransition";
+import { RiArrowDropLeftLine } from "react-icons/ri";
+import { FaRegUserCircle } from "react-icons/fa";
+import NavbarGroup from "@/components/Global/NavbarGroup";
+import BlogDescription from "@/components/Blogs/BeautyofTheStruggleBlog/BlogDescription"
+import { useRouter } from "next/navigation";
+import ThreeColumnFooter from "@/components/Global/LargeBreakpointFooter";
+import Footer from "@/components/Global/Footer";
+
+
+const TheBeautyoftheStruggle = () => {
+  const navigate = useRouter();
+
+  const handleNavigation = () => {
+    navigate.push("/blogs");
+  };
+
+  const ogData = {
+    title: "Your Page Title",
+    description: "Description of your page content.",
+    url: "https://www.yourwebsite.com",
+    image: "https://www.yourwebsite.com/og-image.jpg",
+    siteName: "Your Website Name",
+  };
+  return (
+    <main>
+      <PageTransition>
+        <NavbarGroup />
+
+        <main className="w-full relative h-full  ">
+          {/*Left Side*/}
+          <div className="flex flex-col lg:flex-row pt-[10%] ">
+            <div className=" w-full lg:w-[45%] flex  h-full justify-center  ">
+              <div className="  lg:fixed lg:px-30 flex flex-col gap-5 ">
+                <div
+                  onClick={handleNavigation}
+                  className="flex cursor-pointer  px-10 justify-left items-center sm:pb-5  w-full"
+                >
+                  <RiArrowDropLeftLine className="text-[#FFFFFF] text-opacity-[63%] font-ox text-4xl" />
+                  <p className="text-[#FFFFFF] text-opacity-[63%] uppercase font-ox justify-center items-center text-center ">
+                    Blog
+                  </p>
+                </div>
+
+                <div className="flex px-10 flex-col  justify-center text-left">
+                  <h1 className="text-[#FFFFFF] font-bold text-[30px] text-wrap md:text-[50px] tex lg:text-[57px]  uppercase font-ox">
+                    The Beauty <br />
+                    of the Struggle:
+                  </h1>
+                  <p className="text-[#FFFFFF] text-[20px] text-wrap    text-opacity-[63%]  font-ox">
+                    {" "}
+                    How the Prometheus founder launched an award-winning <br />
+                    marketing rocketship from a small bedroom
+                  </p>
+                </div>
+
+                <div className="flex gap-3 pb-5 px-10">
+                  <FaRegUserCircle className="text-[#FFFFFF] text-[20px]" />
+                  <p className="text-[#FFFFFF] text-[15px] text-opacity-[63%] font-ox">
+                    Prometheus
+                  </p>
+                </div>
+
+          
+              </div>
+            </div>
+
+            {/*Right Side */}
+            <div className=" w-full lg:w-[55%] h-full ">
+              <BlogDescription />
+            </div>
+          </div>
+
+          <footer className="md:hidden">
+            <Footer />
+          </footer>
+
+          <footer className="hidden md:block">
+            <ThreeColumnFooter />
+          </footer>
+        </main>
+      </PageTransition>
+    </main>
+  );
+};
+
+export default TheBeautyoftheStruggle;
---
+++ components/Blogs/BeautyofTheStruggleBlog/BlogDescription.jsx
@@ -0,0 +1,178 @@
+import React from "react";
+
+
+const BlogDescription = () => {
+
+
+  return (
+    <div className=" flex flex-col  w-full justify-center h-auto">
+
+      <div className=" lg:mr-5 py-10">
+        <div className="w-full px-20 pb-10 flex justify-center  items-center ">
+          <img
+            src="/blogs/TheBeautyoftheStruggle/InterstellarGroupPhoto.webp"
+            className="w-[400px] sm:w-full rounded-2xl"
+          />
+        </div>
+        <div className="px-10 sm:px-20 ">
+          <div className="flex flex-col gap-10">
+            <p className="text-[#FFFFFF] sm:text-[28px] font-ox">
+              In 2019, Prometheus&apos; story began with its founder Lcid
+              Fernandez being told by his mentors from various industries,
+              including his father, that a social media marketing agency would
+              never work in Western Visayas, much less Iloilo City. When he
+              pitched his idea for seed capital, he was denied.
+            </p>
+
+            <p className="text-[#FFFFFF] sm:text-[28px]  font-ox">
+              Undaunted, Lcid launched Prometheus from his bedroom with mostly
+              DIY equipment. Then, he was simply Lcid the start-up founder,
+              trying to get his digital marketing company off the ground in the
+              middle of a pandemic.
+            </p>
+            <div className="w-full flex flex-col space-y-4 lg:space-y-0 lg:space-x-4 lg:flex-row justify-center items-center  ">
+              <img
+                src="/blogs/TheBeautyoftheStruggle/LCIDANDGINO2.webp"
+                className="w-[250px] h-[250px] rounded-2xl"
+              />
+              <img
+                src="/blogs/TheBeautyoftheStruggle/lcid-gino3.webp"
+                className="w-[450px] h-[150px] lg:h-[250px] rounded-2xl"
+              />
+            </div>
+            <p className="text-[#FFFFFF] sm:text-[28px]   font-ox">
+              It was during this time as well that he rose to become the VP
+              External of Daily Guardian, a regional daily newspaper. What was a
+              traditional and conservative media outlet quickly became the most
+              innovative media platform in the region. Combining both his roles
+              as Prometheus CEO and Daily Guardian VP External, Lcid created
+              several product lines and content series&apos; that modernized
+              Daily Guardian&apos;s reporting, and introduced a storytelling
+              platform that felt closer to its audiences.
+            </p>
+
+            <p className="text-[#FFFFFF] sm:text-[28px]   font-ox">
+              In this period, Daily Guardian&apos;s digital organic reach grew
+              from 700 to over 10 million people monthly, with his work being
+              recognized in his nomination as Newsmaker of the Year in the Globe
+              Media Excellence Awards. Daily Guardian went on to be perennially
+              recognized by its national and international colleagues for its
+              innovative and responsible journalism.
+            </p>
+
+            <p className="text-[#FFFFFF] sm:text-[28px]   font-ox">
+              Almost five years later, Prometheus grew from its humble 3 digital
+              marketers working out of a bedroom to a team of 50 idea engineers
+              leading the industry in its locality. Prometheus has evolved from
+              a digital marketing agency into becoming the only award-winning
+              full-service marketing agency and public relations firm based in
+              Western Visayas.
+            </p>
+
+            <p className="text-[#FFFFFF] sm:text-[28px]   font-ox">
+              The results that Prometheus has delivered have been recognized not
+              just by its clients, but by the industry itself. Lcid is the first
+              Ilonggo to ever win the Marketing Leader of the Year Award at the
+              Marketing Excellence Awards (MEA) Philippines. In 2023 alone, he
+              also received the following recognitions:
+            </p>
+            <ul className="text-white sm:text-[20px]">
+              <li>
+                - Innovative Entrepreneur of the Year at the Ilonggo
+                Entrepreneur Awards 2023
+              </li>
+              <li>
+                - Dungganon nga Barotacnon Awardee for Entrepreneurship by the
+                Municipality of Barotac Nuevo and JCI Barotac Nuevo Tamasak
+              </li>
+              <li>- Nominated in the ASEAN Business Awards</li>
+              <li>
+                - Nominated in the INJAP Young Ilonggo Entrepreneur Awards
+              </li>
+            </ul>
+            <p className="text-[#FFFFFF] sm:text-[28px]   font-ox">
+              This is now Lcid: the award-winning marketer and entrepreneur, and
+              the leader of the best marketing agency in Western Visayas.
+            </p>
+
+            <div className="w-full pb-10  flex justify-center items-center  ">
+              <img
+                src="/blogs/TheBeautyoftheStruggle/Lcidtrophy2.webp"
+                className="w-[600px] rounded-2xl"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <p className="text-[#FFFFFF] px-10 sm:px-20 sm:text-[28px]  font-ox">
+            Lcid&apos;s story is one of perseverance and defying limitations. He has
+            always said, “The struggle to get from where you are to where you
+            want to be is what makes the journey beautiful. ”
+          </p>
+
+          <p className="text-[#FFFFFF] px-10 sm:px-20 sm:text-[28px]  font-ox">
+            He has led the marketing revolution in Iloilo, evidenced by
+            data-backed conversion strategies and industrial impact. His
+            journey, born from rejection — serves as an inspiration — proving
+            that success often lies beyond the comfort zone, waiting to be
+            seized by those who dare to go through the struggle.
+          </p>
+
+          <div className="w-full pb-10 pt-10  flex justify-center items-center  ">
+            <img
+              src="/blogs/TheBeautyoftheStruggle/PrometheusMothership2.webp"
+              className="w-[600px] rounded-2xl"
+            />
+          </div>
+
+          <p className="text-[#FFFFFF] px-10 sm:px-20 sm:text-[15px]  font-ox">
+            Read Lcid&apos;s column on{" "}
+            <a
+              href="https://dailyguardian.com.ph/category/opinion/prometheus/"
+              className="text-blue-500"
+            >
+              {" "}
+              Daily Guardian
+            </a>{" "}
+            and connect with him on
+            <a
+              href="https://ph.linkedin.com/in/lcidfernandez"
+              className="text-blue-500"
+            >
+              {" "}
+              LinkedIn.
+            </a>{" "}
+          </p>
+
+          <p className="text-[#FFFFFF] px-10 sm:px-20  sm:text-[15px]  font-ox">
+            To learn more about Prometheus, follow its
+            official pages on{" "}
+            <a
+              href="https://www.facebook.com/PrometheusPr"
+              className="text-blue-500"
+            >
+              Facebook{" "}
+            </a>
+            and{" "}
+            <a
+              href="https://www.linkedin.com/company/prometheusph/"
+              className="text-blue-500"
+            >
+              {" "}
+              LinkedIn.
+            </a>{" "}
+          </p>
+          <p className="text-[#FFFFFF] px-10 sm:px-20  sm:text-[15px]  font-ox">
+            Reach Prometheus via email at{" "}
+            <a href="mailto:marketing@prometheus.ph" className="text-blue-500">
+              {" "}
+              marketing@prometheus.ph.
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogDescription;
