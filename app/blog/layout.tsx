f0bbf363413bc442cde9ce8e1b936c73b4bf572f
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-05-14 12:18:50 +0800

    revert back blogs meta dtaa

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 
 import "../globals.css";
 
-// export const metadata: Metadata = {
-//   title: "Blog Page",
-//   description: "Prometheus PH - Blog Page",
+export const metadata: Metadata = {
+  title: "Blog Page",
+  description: "Prometheus PH - Blog Page",
 
-// };
+};
 
 
 export default function RootLayout({
