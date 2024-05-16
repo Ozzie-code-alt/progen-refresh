379fe14ecb81c255e14516aeb04ec0e93ea5acd6
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-05-16 11:29:28 +0800

    Fixed Landing pgae

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import LandingPage from './LandingPage';
 import TransitionComponent from "@/components/Global/TransitionComponent"
+import dynamic from 'next/dynamic';
 export default function Home() {
+
+
+  const LandingPageNOSSR = dynamic( ()=> import('./LandingPage'), {ssr: false})
   return (
     <div className="flex h-auto w-auto flex-col">
       <div className="flex flex-col items-center justify-center">
         <TransitionComponent/>
-        <LandingPage/>
+        <LandingPageNOSSR/>
       </div>
     </div>
   );
