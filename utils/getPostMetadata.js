743ba528951e9f2f2789be621086dcfff91f8399
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-05-24 21:56:08 +0800

    Ples sort

--- utils/getPostMetadata.js
+++ utils/getPostMetadata.js
@@ -1,30 +1,39 @@
-import fs from 'fs'
-import matter from 'gray-matter'
-import path from 'path'
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
 
 export default function getPostMetadata(basePath) {
-    const folder = basePath + '/'
-    const files = fs.readdirSync(folder)
-    const markdownPosts = files.filter(file => file.endsWith('.md'))
+    try {
+        const folder = path.join(basePath); // Use path.join for better cross-platform compatibility
+        const files = fs.readdirSync(folder);
+        const markdownPosts = files.filter(file => file.endsWith('.md'));
 
-    // get the file data
-    const posts = markdownPosts.map((filename) => {
-        const filePath = path.join(folder, filename);
-        const fileContents = fs.readFileSync(`${basePath}/${filename}`, 'utf8')
-        const matterResult = matter(fileContents)
-        const stats = fs.statSync(filePath);  // Get file statistics
-        return {
-            title: matterResult.data.title,
-            prep_time: matterResult.data.prep_time,
-            cook_time: matterResult.data.cook_time,
-            bio: matterResult.data.description,
-            authors: matterResult.data.authors,
-            image: matterResult.data.image,
-            slug: filename.replace('.md', ''),
-            creationDate: stats.birthtime // or stats.ctime based on your system and need
-        }
-    })
+        // Get the file data
+        const posts = markdownPosts.map((filename) => {
+            const filePath = path.join(folder, filename);
+            const fileContents = fs.readFileSync(filePath, 'utf8');
+            const matterResult = matter(fileContents);
+            const stats = fs.statSync(filePath); // Get file statistics
 
-    posts.sort((a, b) => new Date(b.creationDate) - new Date(a.creationDate));
-    return posts
+            return {
+                title: matterResult.data.title,
+                prep_time: matterResult.data.prep_time,
+                cook_time: matterResult.data.cook_time,
+                bio: matterResult.data.description,
+                authors: matterResult.data.authors,
+                image: matterResult.data.image,
+                slug: filename.replace('.md', ''),
+                creationDate: stats.birthtime, // or stats.ctime based on your system and need
+            };
+        });
+
+        // Sort posts by creationDate in descending order
+        posts.sort((a, b) => new Date(b.creationDate) - new Date(a.creationDate));
+
+        return posts;
+
+    } catch (error) {
+        console.error('Error fetching post metadata:', error);
+        return [];
+    }
 }
