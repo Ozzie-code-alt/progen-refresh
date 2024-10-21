f67060f388e82c29bd92b0e2ad7dab6077cd7b65
Kian Aira <90256501+marquisKian@users.noreply.github.com>
2024-10-21 17:08:34 +0800

    feat: add card component linking prominent website

---
+++ components/LandingPage/ProminentCardSection.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import CardContent from "./ProminentCardFormat"
+
+
+const CardSection = () => {
+  return (
+    <CardContent 
+    title = "Transform How your Business Works"
+    content = "Automate your workflow. Focus on the important, The Prominent takes care of the rest."
+    link={{
+        url: "https://theprominent.ph/",
+        text: "Get Started"
+      }}
+    />
+  );
+};
+
+export default CardSection;
