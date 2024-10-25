67a4f68b37a516a81d4b0d4cb67e278e1af15890
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-10-25 15:23:18 +0800

    refactor: card component layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
-import TransitionComponent from "@/components/Global/TransitionComponent"
+import TransitionComponent from '@/components/Global/TransitionComponent';
 import dynamic from 'next/dynamic';
 export default function Home() {
-
-
-  const LandingPageNOSSR = dynamic( ()=> import('./LandingPage'), {ssr: false})
+  const LandingPageNOSSR = dynamic(() => import('./LandingPage'), { ssr: false });
   return (
-    <div className="flex h-auto w-auto flex-col">
-      <div className="flex flex-col items-center justify-center">
-        <TransitionComponent/>
+    <div className='flex h-auto w-auto flex-col'>
+      <div className='flex flex-col items-center justify-center'>
+        <TransitionComponent />
 
-
-        
-        <LandingPageNOSSR/>
-
-
-
-
+        <LandingPageNOSSR />
       </div>
     </div>
   );
